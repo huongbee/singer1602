f98a0631b8a6ca7568079170e3faec98a4f07b93
Huong <huongnguyenak96@gmail.com>
2019-03-17 09:45:29 +0700

    list

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const multer = require('multer')
-
+const SingerSchema = require('./models/Singer')
 const app = express()
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -13,5 +13,17 @@ mongoose.connect('mongodb://localhost:27017/singer1602',{
 mongoose.connection
 .then(()=>console.log('Connected'))
 .catch(e=>console.log(e))
+
+const Singer = mongoose.model('singer',SingerSchema)
+
+app.get('/',(req,res)=>{
+    Singer.find()
+    .then(singers=>{
+        res.render('list',{singers})
+    })
+    .catch(e=>console.log(e.message))
+})
+
+
 
 app.listen(3000,()=>console.log('Server start on port 3000'))
