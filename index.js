3f03322440df6a5124f581feb87cbd04126da2ed
Huong <huongnguyenak96@gmail.com>
2019-03-17 10:20:54 +0700

    upload file

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const multer = require('multer')
 const SingerSchema = require('./models/Singer')
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./public/images')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now()+'-'+file.originalname)
+    }
+})
+const upload = multer({storage})
+
 const app = express()
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -25,6 +36,17 @@ app.get('/',(req,res)=>{
 })
 app.get('/add',(req,res)=>{
     res.render('add')
+})
+app.post('/add',(req,res)=>{
+    upload.single('avatar')(req,res,err=>{
+        if(err){
+            return console.log(err)
+        }
+        res.send({
+            file: req.file,
+            data : req.body
+        })
+    })
 })
 
 
